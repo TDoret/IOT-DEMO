f17ef71323ba088889b27408f3e68f9ff550cbe4
Thomas Doret <thomas.doret33@gmail.com>
2017-02-07 15:24:46 +0100

    add some routes

--- server.js
+++ server.js
@@ -4,13 +4,36 @@ var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
     //res.send('Hello World!');
-    res.render('views/index');
+    res.render('index');
+
+});
+
+app.get('/demo1', function (req, res) {
+    //res.send('Hello World!');
+    res.render('demo1');
+
+});
+
+app.get('/demo2', function (req, res) {
+    //res.send('Hello World!');
+    res.render('demo2');
 
 });
 
 app.get('/textAnalytics', function (req, res) {
     res.send('welcome to textAnalyticsComponent');
 });
+
+var swig = require('swig');
+
+// This is where all the magic happens!
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.set('view cache', false);
+swig.setDefaults({cache: false});
+
 
 var server = app.listen(port, function () {
     var host = server.address().address;
